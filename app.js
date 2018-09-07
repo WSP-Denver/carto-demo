4805f9ca156ec0f13c29f5b9e94db484765f8d47
jgipper <josh@kritek.org>
2018-09-07 08:35:37 -0600

    gtfs-rt data, transition to node app

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+var requestSettings = {
+  method: 'GET',
+  username = "RTDgtfsRT",
+  password = "realT!m3Feed",
+  url = "http://" + username + ":" + password + "@www.rtd-denver.com/google_sync";
+  encoding: null
+};
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
